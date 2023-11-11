a53b661662ff34e09d96651ef5cdc70c77d99833
Hermann AGOSSOU <hermannagossou6@gmail.com>
2023-11-11 04:30:48 +0100

    deployment shit

--- run-deploy-to-master.sh
+++ run-deploy-to-master.sh
@@ -1,3 +1,6 @@
+git add . && git commit -m "pre-prod" && git push
+
+cd web-source
 cd web
 cd ..
 deactivate
@@ -5,9 +8,9 @@ rm -rf venv/
 conda activate web-dev
 python -m venv venv
 source venv/bin/activate
-git checkout source && git add . && git commit -m "pre-prod" && git push
-cd web
+git checkout source && git add . && git commit -m "pre-prod" && git push && 
 
+cd web-source
 pip install -r requirements-full.txt
 git branch -D master
 git checkout source
@@ -15,6 +18,7 @@ git checkout -b master
 mkdocs build
 mv site ../tmp-dir
 cd ..
+rm -r web-source
 rm -r web
 mv tmp-dir web
 git add .
